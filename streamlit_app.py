0615ed5397f38d86af934a10fa7c8686e69a81b9
Lynsoo <157243525+Lynsoo@users.noreply.github.com>
2025-01-29 17:54:07 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,21 @@
 import streamlit as st
+import torch
+import torch.nn as nn
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+import seaborn as sns
+import math
+import yfinance as yf
+
+from torch.utils.data import Dataset, DataLoader
+from datetime import date, timedelta, datetime
 
 st.title('Stock Prediction App')
 
 st.info('This is a stock prediction app')
 
 ticker = st.text_input("Company's Ticker", placeholder ="Enter Ticker")
+df_ticker= [ticker]
+df = yf.download( df_ticker, period = 'max')
